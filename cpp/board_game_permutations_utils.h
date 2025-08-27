359f430ad6f568218c62bf6664043b980a9e185a
darkfirexxi <aselz21@gmail.com>
2025-08-26 21:19:45 -0400

    cpp format

--- cpp/board_game_permutations_utils.h
+++ cpp/board_game_permutations_utils.h
@@ -4,30 +4,26 @@
 #include <algorithm>
 #include <cmath>
 #include <filesystem>
+#include <fstream>
 #include <future>
 #include <iostream>
-#include <fstream>
 #include <vector>
 
 using Board = std::vector<std::vector<int>>;
 
-void print_board(const Board& board);
+void print_board(const Board &board);
 
 Board rotate_board_90(Board board);
 
-Board generate_initial_board(const int& size);
+Board generate_initial_board(const int &size);
 
-std::string hash_board(const Board& board);
+std::string hash_board(const Board &board);
 
-int get_file_item_count(std::vector<std::string> files, std::string folder_name);
+int get_file_item_count(std::vector<std::string> files,
+                        std::string folder_name);
 
-void display_round_stats(
-    int round,
-    int rounds,
-    std::time_t start,
-    std::time_t round_start,
-    int new_board_count,
-    int result_count
-);
+void display_round_stats(int round, int rounds, std::time_t start,
+                         std::time_t round_start, int new_board_count,
+                         int result_count);
 
 #endif
